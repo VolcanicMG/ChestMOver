beba0db3ea2de4b8a67ec9d10a5ca6848e35f67c
Mirsario <me@mirsar.io>
2021-04-17 22:07:28 +0300

    Unfinished networking.

---
+++ Common/Packets/AttemptChestPickupPacket.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using ChestMover.Core.Networking;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace ChestMover.Common.Packets
+{
+	/// <summary>
+	/// Sent by players that attempt to pickup a chest. <br/>
+	/// If successful, the chest's contents are sent to the player who picked it up, and all others players get a short version of that packet, without the items.
+	/// </summary>
+	public class AttemptChestPickupPacket : NetPacket
+	{
+		public AttemptChestPickupPacket(Point16 chestPosition)
+		{
+			Writer.Write(chestPosition.X);
+			Writer.Write(chestPosition.Y);
+		}
+
+		public override void Read(BinaryReader reader, int sender)
+		{
+			Point16 chestPos = new Point16(reader.ReadInt16(), reader.ReadInt16());
+
+			if (Main.netMode != NetmodeID.Server)
+			{
+				return;
+			}
+
+			int chestId = Chest.FindChest(chestPos.X, chestPos.Y);
+
+			if (!Main.chest.IndexInRange(chestId))
+			{
+				return;
+			}
+
+			var player = Main.player[sender];
+
+			if (player?.active != true)
+			{
+				return;
+			}
+
+			var chest = Main.chest[chestId];
+
+			player.GetModPlayer<ChestMoverPlayer>().PickupChest(chest);
+
+			MultiplayerSystem.SendPacket(new ChestPickupConfirmationPacket(chest), toClient: sender);
+			MultiplayerSystem.SendPacket(new VisualChestPickupPacket((byte)sender, chest), ignoreClient: sender);
+		}
+	}
+}
---
+++ Core/Networking/MultiplayerSystem.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChestMover.Core.Networking
+{
+	public static class MultiplayerSystem
+	{
+		private static List<NetPacket> packets;
+		private static Dictionary<Type, NetPacket> packetsByType;
+
+		private static Mod modInstance;
+
+		//Get
+		public static NetPacket GetPacket(byte id) => packets[id];
+
+		public static NetPacket GetPacket(Type type) => packetsByType[type];
+
+		public static T GetPacket<T>() where T : NetPacket => ModContent.GetInstance<T>();
+
+		//Send
+		public static void SendPacket<T>(T packet, int toClient = -1, int ignoreClient = -1, Func<Player, bool> sendDelegate = null) where T : NetPacket
+		{
+			if (Main.netMode == NetmodeID.SinglePlayer)
+			{
+				return;
+			}
+
+			ModPacket modPacket = modInstance.GetPacket();
+
+			modPacket.Write((byte)packet.Id);
+			packet.WriteAndDispose(modPacket);
+
+			try {
+				if (Main.netMode == NetmodeID.MultiplayerClient)
+				{
+					modPacket.Send();
+				}
+				else if (toClient != -1)
+				{
+					modPacket.Send(toClient, ignoreClient);
+				}
+				else
+				{
+					for (int i = 0; i < Main.player.Length; i++)
+					{
+						var player = Main.player[i];
+
+						if (i != ignoreClient && Netplay.Clients[i].State >= 10 && (sendDelegate?.Invoke(player) ?? true))
+						{
+							modPacket.Send(i);
+						}
+					}
+				}
+			}
+			catch { }
+		}
+
+		internal static void Load(Mod mod)
+		{
+			modInstance = mod;
+
+			packets = new List<NetPacket>();
+			packetsByType = new Dictionary<Type, NetPacket>();
+
+			foreach (var type in Assembly.GetExecutingAssembly().GetTypes().Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(NetPacket))))
+			{
+				var instance = (NetPacket)FormatterServices.GetUninitializedObject(type);
+
+				instance.Id = packets.Count;
+				packetsByType[type] = instance;
+
+				packets.Add(instance);
+
+				ContentInstance.Register(instance);
+			}
+		}
+
+		internal static void Unload()
+		{
+			modInstance = null;
+
+			if (packets != null)
+			{
+				packets.Clear();
+
+				packets = null;
+			}
+
+			if (packetsByType != null)
+			{
+				packetsByType.Clear();
+
+				packetsByType = null;
+			}
+		}
+
+		internal static void HandlePacket(BinaryReader reader, int sender)
+		{
+			try
+			{
+				byte packetId = reader.ReadByte();
+
+				if (packetId > packets.Count)
+				{
+					return;
+				}
+
+				var packet = packets[packetId];
+
+				packet.Read(reader, sender);
+			}
+			catch
+			{
+				//TODO: Log
+			}
+		}
+	}
+}
